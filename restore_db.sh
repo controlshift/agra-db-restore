274e46cf825ae5ee1429ae4aa1869b12e32daa06
Jacinda Moore <alynna.kasmira@gmail.com>
2014-08-13 14:31:49 -0400

    restore db: use a parameterized function

--- restore_db.sh
+++ restore_db.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Takes a single argument: an ISO-8601-formatted date, like 1964-07-02
+retrieve_and_unpack_backups() {
+  local TARGET_DATE=$1
+
+  aws s3 cp s3://agra-db-backups/backups/db2.agra.managedmachine.com-data-${TARGET_DATE}.tar.gz /tmp/db2.agra.managedmachine.com-data-${TARGET_DATE}.tar.gz
+  aws s3 cp s3://agra-db-backups/backups/db2.agra.managedmachine.com-xlog-${TARGET_DATE}.tar.gz /tmp/db2.agra.managedmachine.com-xlog-${TARGET_DATE}.tar.gz
+
+  sudo tar -zxvf /tmp/db2.agra.managedmachine.com-data-${TARGET_DATE}.tar.gz -C /var/lib/postgresql/9.2
+  sudo tar -zxvf /tmp/db2.agra.managedmachine.com-xlog-${TARGET_DATE}.tar.gz -C /var/lib/postgresql/9.2
+}
+
 set -x  # Echo commands and expand variables
 
 source /vagrant/.env
@@ -12,12 +24,8 @@ sudo service postgresql stop
 # move aside the old pg files
 mv /var/lib/postgresql/9.2/main /var/lib/postgresql/9.2/main.old
 
-
 # get the backup file 
-aws s3 cp s3://agra-db-backups/backups/db2.agra.managedmachine.com-data-2014-03-24.tar.gz /tmp/db2.agra.managedmachine.com-data-2014-03-24.tar.gz
-aws s3 cp s3://agra-db-backups/backups/db2.agra.managedmachine.com-xlog-2014-03-24.tar.gz /tmp/db2.agra.managedmachine.com-xlog-2014-03-24.tar.gz
+retrieve_and_unpack_backups 2014-03-24
 
-sudo tar -zxvf /tmp/db2.agra.managedmachine.com-data-2014-03-24.tar.gz -C /var/lib/postgresql/9.2
-sudo tar -zxvf /tmp/db2.agra.managedmachine.com-xlog-2014-03-24.tar.gz -C /var/lib/postgresql/9.2
-
+# bring the db back up
 sudo service postgresql start
