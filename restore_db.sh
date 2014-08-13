14bb473d872da91e9bd11b5ec704a115ff45c319
Jacinda Moore <alynna.kasmira@gmail.com>
2014-08-13 15:33:59 -0400

    date parameter; use default pg conf location if not set; use sudo as necessary

--- restore_db.sh
+++ restore_db.sh
@@ -11,21 +11,37 @@ retrieve_and_unpack_backups() {
   sudo tar -zxvf /tmp/db2.agra.managedmachine.com-xlog-${TARGET_DATE}.tar.gz -C /var/lib/postgresql/9.2
 }
 
-set -x  # Echo commands and expand variables
+### Main script ###
+USAGE="Usage: $0 DATE"
 
+# Echo commands and expand variables
+set -x
+
+# Load environment variables
 source /vagrant/.env
 
+# Extract date parameter
+if [ -z $1 ]; then
+  echo $USAGE
+  exit
+fi
+DATABASE_DATE=$1
+
 # Edit postgresql.conf to change listen address to '*':
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+if [ -z $PG_CONF ]; then
+  echo "Using default pg conf location"
+  PG_CONF="/etc/postgresql/9.2/main/postgresql.conf"
+fi
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
 
 # stop while we mess around
 sudo service postgresql stop
 
 # move aside the old pg files
-mv /var/lib/postgresql/9.2/main /var/lib/postgresql/9.2/main.old
+sudo mv /var/lib/postgresql/9.2/main /var/lib/postgresql/9.2/main.old
 
 # get the backup file 
-retrieve_and_unpack_backups 2014-03-24
+retrieve_and_unpack_backups $DATABASE_DATE
 
 # bring the db back up
 sudo service postgresql start
